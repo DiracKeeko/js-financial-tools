bec6612bcd107bf4794c2da2e1583d36fa7b83c6
Keeko <787018661@qq.com>
2022-03-03 16:17:40 +0800

    [feat] add nyc to get test coverage

--- test/src/calc/number.test.js
+++ test/src/calc/number.test.js
@@ -3,54 +3,58 @@ import assert from "assert";
 
 describe("number test", () => {
 
-  it("isRealNumber(undefined)", () => {
-    const res = number.isRealNumber(undefined);
-    assert(res === false);
-  });
-  it("isRealNumber(null)", () => {
-    const res = number.isRealNumber(null);
-    assert(res === false);
-  });
-  it("isRealNumber(NaN)", () => {
-    const res = number.isRealNumber(NaN);
-    assert(res === false);
-  });
-  it("isRealNumber(string)", () => {
-    const res = number.isRealNumber("33");
-    assert(res === false);
-  });
-  it("isRealNumber(number float)", () => {
-    const res = number.isRealNumber(1.1);
-    assert(res === true);
-  });
-  it("isRealNumber(number int)", () => {
-    const res = number.isRealNumber(22);
-    assert(res === true);
+  describe("isRealNumber() test", () => {
+    it("isRealNumber(undefined)", () => {
+      const res = number.isRealNumber(undefined);
+      assert(res === false);
+    });
+    it("isRealNumber(null)", () => {
+      const res = number.isRealNumber(null);
+      assert(res === false);
+    });
+    it("isRealNumber(NaN)", () => {
+      const res = number.isRealNumber(NaN);
+      assert(res === false);
+    });
+    it("isRealNumber(string)", () => {
+      const res = number.isRealNumber("33");
+      assert(res === false);
+    });
+    it("isRealNumber(number float)", () => {
+      const res = number.isRealNumber(1.1);
+      assert(res === true);
+    });
+    it("isRealNumber(number int)", () => {
+      const res = number.isRealNumber(22);
+      assert(res === true);
+    });
   });
 
-  it("float(undefined)", () => {
-    const res = number.float(undefined);
-    assert(res === "--");
-  });
-  it("float(null)", () => {
-    const res = number.float(null);
-    assert(res === "--");
-  });
-  it("float(NaN)", () => {
-    const res = number.float(NaN);
-    assert(res === "--");
-  });
-  it("float(string)", () => {
-    const res = number.float("33");
-    assert(res === "--");
-  });
-  it("float(number float)", () => {
-    const res = number.float(1.1);
-    assert(res === "1.10");
-  });
-  it("float(number int)", () => {
-    const res = number.float(22);
-    assert(res === "22.00");
+  describe("float() test", () => {
+    it("float(undefined)", () => {
+      const res = number.float(undefined);
+      assert(res === "--");
+    });
+    it("float(null)", () => {
+      const res = number.float(null);
+      assert(res === "--");
+    });
+    it("float(NaN)", () => {
+      const res = number.float(NaN);
+      assert(res === "--");
+    });
+    it("float(string)", () => {
+      const res = number.float("33");
+      assert(res === "--");
+    });
+    it("float(number float)", () => {
+      const res = number.float(1.1);
+      assert(res === "1.10");
+    });
+    it("float(number int)", () => {
+      const res = number.float(22);
+      assert(res === "22.00");
+    });
   });
 
 })
