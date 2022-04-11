df2b1373055b27842ae0f563536f868559365066
Keeko <787018661@qq.com>
2022-04-11 15:17:10 +0800

    [feat] add checkQuarterInRange, checkYearInRange

--- src/util/acquire.js
+++ src/util/acquire.js
@@ -1,5 +1,11 @@
 import { isRealNumber } from "@/calc/number";
 
+import {
+  getDateTime,
+  wholeQuarterRangeMap,
+  wholeYearRangeArr,
+} from "./constant";
+
 function getIntPartLength(num) {
   return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
@@ -18,19 +24,79 @@ function getMonetaryUnit(val) {
   return "";
 }
 
+/**
+ *
+ * @param {Array} dateArr
+ * case1: dateArr = ["2022-01-01", "2019-10-12", "2018-05-18"];
+ * @returns maxDateStr
+ * case1 result: "2022-01-01"
+ */
 function getMaxDate(dateArr) {
-  // arr = ["2022-01-01", "2019-10-12", "2018-05-18"];
-  const timestampArr = dateArr.map((item) => new Date(item).getTime());
+  const timestampArr = dateArr.map((item) => getDateTime(item));
   const maxTimestamp = Math.max(...timestampArr);
   const maxTimeIndex = timestampArr.indexOf(maxTimestamp);
   return dateArr[maxTimeIndex];
 }
 
 function getMinDate(dateArr) {
-  const timestampArr = dateArr.map((item) => new Date(item).getTime());
+  const timestampArr = dateArr.map((item) => getDateTime(item));
   const minTimestamp = Math.min(...timestampArr);
   const minTimeIndex = timestampArr.indexOf(minTimestamp);
   return dateArr[minTimeIndex];
 }
 
-export { getIntPartLength, getMonetaryUnit, getMaxDate, getMinDate };
+/**
+ *
+ * @param {String|Number} year
+ * case1: "2020"
+ * @param {String} quarter
+ * case1: "Q1"
+ * @param {Array} dateRange = [startDate, endDate]
+ * case1: ["2020-01-01", "2021-11-11"]
+ *
+ * @returns Boolean
+ */
+function checkQuarterInRange(year, quarter, [startDate, endDate]) {
+  const startTimeStamp = getDateTime(startDate);
+  const endTimeStamp = getDateTime(endDate);
+  const quarterStartDate = `${year}-${wholeQuarterRangeMap[quarter][0]}`;
+  const quarterEndDate = `${year}-${wholeQuarterRangeMap[quarter][1]}`;
+  const quarterStartTimeStamp = getDateTime(quarterStartDate);
+  const quarterEndTimeStamp = getDateTime(quarterEndDate);
+  if (
+    quarterStartTimeStamp >= startTimeStamp &&
+    quarterStartTimeStamp <= endTimeStamp &&
+    quarterEndTimeStamp >= startTimeStamp &&
+    quarterEndTimeStamp <= endTimeStamp
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function checkYearInRange(year, [startDate, endDate]) {
+  const startTimeStamp = getDateTime(startDate);
+  const endTimeStamp = getDateTime(endDate);
+  const yearStartDate = `${year}-${wholeYearRangeArr[0]}`;
+  const yearEndDate = `${year}-${wholeYearRangeArr[1]}`;
+  const yearStartTimeStamp = getDateTime(yearStartDate);
+  const yearEndTimeStamp = getDateTime(yearEndDate);
+  if (
+    yearStartTimeStamp >= startTimeStamp &&
+    yearStartTimeStamp <= endTimeStamp &&
+    yearEndTimeStamp >= startTimeStamp &&
+    yearEndTimeStamp <= endTimeStamp
+  ) {
+    return true;
+  }
+  return false;
+}
+
+export {
+  getIntPartLength,
+  getMonetaryUnit,
+  getMaxDate,
+  getMinDate,
+  checkQuarterInRange,
+  checkYearInRange,
+};
--- test/src/util/acquire.test.js
+++ test/src/util/acquire.test.js
@@ -1,7 +1,7 @@
 import { acquire } from "@/util";
 import assert from "assert";
 
-const { getIntPartLength, getMonetaryUnit, getMaxDate, getMinDate } = acquire;
+const { getIntPartLength, getMonetaryUnit, getMaxDate, getMinDate, checkQuarterInRange, checkYearInRange } = acquire;
 
 describe("acquire test", () => {
   describe("getIntPartLength", () => {
@@ -45,6 +45,28 @@ describe("acquire test", () => {
     it("getMinDate 01", () => {
       const dateArr = ["2022-01-01", "2019-10-12", "2018-05-18"]
       assert(getMinDate(dateArr) === "2018-05-18");
+    });
+  });
+
+  describe("checkQuarterInRange", () => {
+    it("checkQuarterInRange 01", () => {
+      const dateRange = ["2021-12-12", "2022-04-06"];
+      assert(checkQuarterInRange(2022, "Q1", dateRange) === true);
+    });
+    it("checkQuarterInRange 02", () => {
+      const dateRange = ["2021-12-12", "2022-04-06"];
+      assert(checkQuarterInRange(2021, "Q4", dateRange) === false);
+    });
+  });
+
+  describe("checkYearInRange", () => {
+    it("checkYearInRange 01", () => {
+      const dateRange = ["2020-12-12", "2022-04-06"];
+      assert(checkYearInRange(2021, dateRange) === true);
+    });
+    it("checkYearInRange 02", () => {
+      const dateRange = ["2020-12-12", "2022-04-06"];
+      assert(checkYearInRange(2020, dateRange) === false);
     });
   });
 
